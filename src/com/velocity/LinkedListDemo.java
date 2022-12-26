a57796ef586462484a6d8d3aaa7bbf4204f1afab
roshanichoudhary96 <roshanichoudhary96@gmail.com>
2022-12-26 13:20:22 +0530

    second commit : added linkedList

---
+++ src/com/velocity/LinkedListDemo.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(100);
+		list.add(200);
+		list.add(300);
+		list.add(400);
+		for(Integer i:list) {
+			System.out.println(i);
+		}
+
+	}
+
+}
