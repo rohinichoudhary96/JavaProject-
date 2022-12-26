a6738c918aa2f7998aa3dce2c53a7a0a1747ecf7
roshanichoudhary96 <roshanichoudhary96@gmail.com>
2022-12-26 21:51:13 +0530

    data is added

---
+++ src/com/rohini/LinkedListDemo.java
@@ -0,0 +1,21 @@
+package com.rohini;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(100);
+		list.add(200);
+		list.add(300);
+		list.add(400);
+		/*for(Integer i:list) {
+			System.out.println(i);*/
+		System.out.println(list);
+			
+		}
+
+	}
+
+
