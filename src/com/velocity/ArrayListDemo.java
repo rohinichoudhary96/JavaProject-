7a95bb67fb5381db7b09b6b71fed91e925253bb0
roshanichoudhary96 <roshanichoudhary96@gmail.com>
2022-12-26 12:51:01 +0530

    first commit : done

---
+++ src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Rohini");
+		list.add("Sandip");
+		list.add("Akshay");
+		list.add("Swati");
+		list.add("Agastya");
+		
+		list.stream().forEach(x->System.out.println(x));
+
+	}
+
+}
